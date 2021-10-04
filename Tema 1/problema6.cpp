db1c600c0ea17c3afcc2b4f235370aac7fee97e0
d22tny <d22tny@gmail.com>
2021-10-04 23:26:19 +0300

    problema 6 patratele

--- Tema 1/problema6.cpp
+++ Tema 1/problema6.cpp
@@ -88,38 +88,69 @@ int main()
 Pseudo
 
 citeste a,b,c,d,e
-daca a >= b AND a >= c AND a >= d AND a >= e atunci max <- a 
-                                                    a <- 0
-daca b >= a AND b >= c AND b >= d AND b >= e atunci max <- b
-                                                    b <- 0
-daca c >= a AND c >= b AND c >= d AND c >= e atunci max <- c
-                                                    c <- 0
-daca d >= a AND d >= b AND d >= c AND d >= e atunci max <- d
-                                                    d <- 0
-daca e >= a AND e >= b AND e >= c AND e >= d atunci max <- e
-                                                    e <- 0
 
-daca a >= b AND a >= c AND a >= d AND a >= e atunci max2 <- a 
-                                                    a <- 0
-daca b >= a AND b >= c AND b >= d AND b >= e atunci max2 <- b
-                                                    b <- 0
-daca c >= a AND c >= b AND c >= d AND c >= e atunci max2 <- c
-                                                    c <- 0
-daca d >= a AND d >= b AND d >= c AND d >= e atunci max2 <- d
-                                                    d <- 0
-daca e >= a AND e >= b AND e >= c AND e >= d atunci max2 <- e
-                                                    e <- 0
-                                        
-daca a >= b AND a >= c AND a >= d AND a >= e atunci max3 <- a 
-                                                    a <- 0
-daca b >= a AND b >= c AND b >= d AND b >= e atunci max3 <- b
-                                                    b <- 0
-daca c >= a AND c >= b AND c >= d AND c >= e atunci max3 <- c
-                                                    c <- 0
-daca d >= a AND d >= b AND d >= c AND d >= e atunci max3 <- d
-                                                    d <- 0
-daca e >= a AND e >= b AND e >= c AND e >= d atunci max3 <- e
-                                                    e <- 0
+┌daca a >= b AND a >= c AND a >= d AND a >= e atunci 
+│     max <- a 
+│     a <- 0
+└■                                                    
+┌daca b >= a AND b >= c AND b >= d AND b >= e atunci 
+│     max <- b
+│     b <- 0
+└■                                                    
+┌daca c >= a AND c >= b AND c >= d AND c >= e atunci 
+│     max <- c
+│     c <- 0
+└■                                                    
+┌daca d >= a AND d >= b AND d >= c AND d >= e atunci 
+│     max <- d
+│     d <- 0
+└■                                                    
+┌daca e >= a AND e >= b AND e >= c AND e >= d atunci 
+│     max <- e
+│     e <- 0
+└■                                                    
+
+┌daca a >= b AND a >= c AND a >= d AND a >= e atunci 
+│     max2 <- a 
+│     a <- 0
+└■                                                    
+┌daca b >= a AND b >= c AND b >= d AND b >= e atunci 
+│     max2 <- b
+│     b <- 0
+└■                                                    
+┌daca c >= a AND c >= b AND c >= d AND c >= e atunci 
+│     max2 <- c
+│     c <- 0
+└■                                                    
+┌daca d >= a AND d >= b AND d >= c AND d >= e atunci 
+│     max2 <- d
+│     d <- 0
+└■                                                    
+┌daca e >= a AND e >= b AND e >= c AND e >= d atunci 
+│     max2 <- e
+│     e <- 0
+└■   
+
+┌daca a >= b AND a >= c AND a >= d AND a >= e atunci 
+│     max3 <- a 
+│     a <- 0
+└■                                                    
+┌daca b >= a AND b >= c AND b >= d AND b >= e atunci 
+│     max3 <- b
+│     b <- 0
+└■                                                    
+┌daca c >= a AND c >= b AND c >= d AND c >= e atunci 
+│     max3 <- c
+│     c <- 0
+└■                                                    
+┌daca d >= a AND d >= b AND d >= c AND d >= e atunci 
+│     max3 <- d
+│     d <- 0
+└■                                                    
+┌daca e >= a AND e >= b AND e >= c AND e >= d atunci 
+│     max3 <- e
+│     e <- 0
+└■
 
 scrie max + max2 + max3
 
