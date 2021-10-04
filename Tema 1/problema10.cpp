4961e8eb51f97510c013b6615b300ce80f88a39e
d22tny <d22tny@gmail.com>
2021-10-04 21:47:56 +0300

    problema10

---
+++ Tema 1/problema10.cpp
@@ -0,0 +1,20 @@
+/*10. Se dă un număr natural nenul n. Să se determine cel mai mic număr natural, mai
+mare sau egal decât n, care are ultimele două cifre egale cu 0.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, rest;
+    cout <<"Introduceti numarul\n";
+    cin >> n;
+    rest = n%100;
+    if(rest == 0) {
+        cout << "Numarul dat este deja un numar care are ultimele doua cifre egale cu zero";
+    } else {
+        cout << "Numarul mai mare decat " << n << " care are ultimele doua cifre egale cu zero este" << 100 + n - r;
+    }
+}
