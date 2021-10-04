f57a3a286d6b2acb941bb6dc548cb320fb70c162
d22tny <d22tny@gmail.com>
2021-10-04 19:57:36 +0300

    problema4

---
+++ Tema 1/problema4.cpp
@@ -0,0 +1,24 @@
+/*4. Fiind date vârstele a doi copii afișați care dintre ei este cel mai mare și cu cât.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Introduceti varsta primului copil\n";
+    cin>>a;
+    cout<<"Introduceti varsta celui de al doilea copil\n";
+    cin>>b;
+    if (a>0 && b>0) {
+        if (a<=b) {
+            cout <<"Al doilea copil este mai mare decat primul copil, fiind o diferenta de varsta de " << b - a << " ani.";
+        } else {
+            cout <<"Primul copil este mai mare decat al doilea copil, fiind o diferenta de varsta de " << a - b << " ani.";
+        }
+    } else {
+        cout << "Varstele introduse nu sunt corecte!";
+    }
+}
